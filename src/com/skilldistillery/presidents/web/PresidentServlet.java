1e3d74628096f4708da241313ed218bc71a3ef50
dclarkasu <dclark205@gmail.com>
2017-09-11 17:11:13 -0600

    added HTTPSession

--- src/com/skilldistillery/presidents/web/PresidentServlet.java
+++ src/com/skilldistillery/presidents/web/PresidentServlet.java
@@ -1,42 +1,59 @@
 package com.skilldistillery.presidents.web;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.skilldistillery.presidents.data.PresidentOBJ;
+
+
 
 /**
  * Servlet implementation class PresidentServlet
  */
 @WebServlet("/PresidentServlet")
 public class PresidentServlet extends HttpServlet {
-    private PresidentFileDAO presidentDAO;
-    private int i = 1;
+    private PresidentDAO presidentDAO;
 
-    public void init() throws ServletException { //RUNS AUTOMATICALLY
-        presidentDAO = new PresidentFileDAO(getServletContext());
+    public void init() throws ServletException {
+      presidentDAO = new PresidentFileDAO(getServletContext());
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        
+        HttpSession session = request.getSession();
+        
+        Integer i = (Integer)session.getAttribute("i");
+        if(i==null) {
+            i = 1;
+        }
+        
+        List<PresidentOBJ> current = (List<PresidentOBJ>)session.getAttribute("current");
+        if(current==null) {
+            current = presidentDAO.getFullList();
+        }
+        
         try {
-            if ((isNotNullOrEmpty(request.getParameter("selection")))){  //CHECK IF NOT NULL & BUTTON SELECTION
+            if ((isNotNullOrEmpty(request.getParameter("selection")))){
                 String radioButton = request.getParameter("search");
                 String userInput = request.getParameter("selection");
-                
-                switch(radioButton) {   //CHECK WHICH BUTTON USER SELECTED
-                case "party":   //USER SELECTED PARTY
-                    presidentDAO.setCurrent(presidentDAO.getFilterList(userInput));
-                    if (presidentDAO.getCurrent().size() == 0) {
-                        presidentDAO.setCurrent(presidentDAO.getFullList());
+                switch(radioButton) {
+                case "party":
+                    current = presidentDAO.getFilterList(userInput);
+                    if (current.size() == 0) {
+                        current = presidentDAO.getFullList();
                     }
                     i = 1;
                     break;
-                case "term":   //USER SELECTED TERM BUTTON
-                    presidentDAO.setCurrent(presidentDAO.getFullList());
+                case "term":
+                    current = presidentDAO.getFullList();
                     i = Integer.parseInt(userInput);
                     if (i <= 0 || i > (presidentDAO.getFullList().size())) {
                         i = 1;
@@ -46,36 +63,46 @@ public class PresidentServlet extends HttpServlet {
                     i = 1;
                 }
             } 
-            else if ((isNotNullOrEmpty(request.getParameter("full")))) {  //RESET FULL LIST HERE WHEN USER SELECTS BUTTON
-                presidentDAO.setCurrent(presidentDAO.getFullList());
+            else if ((isNotNullOrEmpty(request.getParameter("full")))) {
+                current = presidentDAO.getFullList();
                 i = 1;
             }
-            else if (isNotNullOrEmpty(request.getParameter("next"))) {  //USER SELECTED NEXT BUTTON
-                if (i == presidentDAO.getCurrent().size()) {
+            else if (isNotNullOrEmpty(request.getParameter("next"))) {
+                if (i == current.size()) {
                     i = 1;
                 } 
                 else {
                     ++i;
                 }
             } 
-            else if (isNotNullOrEmpty(request.getParameter("previous"))) {  //USER SELECTED PREVIOUS BUTTON
+            else if (isNotNullOrEmpty(request.getParameter("previous"))) {
                 if (i == 1) {
-                    i = presidentDAO.getCurrent().size();
+                    i = current.size();
                 } else {
                     --i;
                 }
             }
         }
-        catch (Exception e) { //IF USER DID NOT PUT IN AN INTEGER, IT WILL SHOW GEORGE WASHINGTON
+        catch (Exception e) {
             i = 1;
         }
-
-        request.setAttribute("currentPresident", presidentDAO.getCurrent().get(i - 1)); // TELL JSP, THE WORD "CURRENT PRESIDENT, REFERS TO THE SECOND ARG
-        request.getRequestDispatcher("/presidentsdata.jsp").forward(request, response); // SENDS INFO TO JSP
-
+        
+        session.setAttribute("i", i);
+        session.setAttribute("current", current);
+        session.setAttribute("currentPresident", current.get(i - 1));
+        request.getRequestDispatcher("/presidentsdata.jsp").forward(request, response); 
+        
     }
 
-    private boolean isNotNullOrEmpty(String op) {  //METHOD THAT CHECKS IF USER INPUT IS NULL
+    private boolean isNotNullOrEmpty(String op) {
         return op != null && op.length() > 0;
     }
+
+//     protected void doPost(HttpServletRequest request, HttpServletResponse
+//     response)
+//     throws ServletException, IOException {
+//         doGet(request, response);
+//    
+//     }
+
 }
